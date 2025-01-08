d4b8b75c33cbac452e67ff15b85b6b38329e3814
endrin <158015+endrin@users.noreply.github.com>
2025-01-08 15:48:33 +0200

    Draw the rest of the owl

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import string
+
+from collections import Counter
+
+
+example = "books/frankenstein.txt"
+
+
+def main():
+    print_report_on(example)
+
+
+def frequencies(line: str) -> Counter[str]:
+    normalized_line = line.strip().lower()
+    chars = (c for c in normalized_line if c in string.ascii_lowercase)
+
+    return Counter(chars)
+
+
+def print_report_on(src: os.PathLike):
+    def second_in_pair(p: tuple[str, int]) -> int:
+        (_, snd) = p
+        return snd
+
+    print(f"--- Begin report of {src} ---")
+
+    with open(src) as f:
+        content = f.read()
+
+    words = content.split()
+    print(f"{len(words)} words found in the document")
+    print()
+
+    freqs = frequencies(content)
+    for char, amount in sorted(freqs.items(), key=second_in_pair, reverse=True):
+        print(f"The '{char}' character was found {amount} times")
+
+    print("--- End report ---")
+
+
+main()
